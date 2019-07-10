376f2c643300fdd838c6e247524613a2c6ed4ac3
Shane Miller <shanemiller89@gmail.com>
2019-07-10 11:25:53 -0500

    added factory func and started components

---
+++ src/scripts/components.js
@@ -0,0 +1,17 @@
+const COMPONENT = {
+    poiFormComponent: function () {
+        let poiForm = `
+        
+        `
+    },
+    poiDisplayComponent: function () {
+        let poiDisplay = `
+        <h1> Points of Interest</h1>
+        <h2>Location: </h2><div></div>
+        <h2>P.O.I Name: </h2><div></div>
+        <h2>Description: </h2><div></div>
+        <h2>Cost: </h2><div></div>
+        <h2>Review: </h2><div></div>
+        `
+    }
+};
